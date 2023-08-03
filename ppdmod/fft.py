a70b9c2f86aef4248360570f8e033ff75d53a5d3
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-08-03 19:13:39 +0200

    Work on `fft`

--- ppdmod/fft.py
+++ ppdmod/fft.py
@@ -26,56 +26,59 @@ def compute_2Dfourier_transform(image: np.ndarray) -> np.ndarray:
     return np.fft.fftshift(pyfftw.FFTW(ft_input, ft_output, axes=(-2, -1))())
 
 
-def get_frequency_axis(dim: int, pixel_size: float, wavelength: float) -> np.ndarray:
+def get_frequency_axis(dim: int,
+                       pixel_size: u.mas, wavelength: u.um) -> np.ndarray:
     """Calculates the frequency axis in meters at the observed wavelength.
 
     Parameters
     ----------
     dim : float
         The dimension [px].
-    pixel_size : float
-        The pixel size [u.rad].
-    wavelength : numpy.ndarray
-        The wavelength [u.m].
+    pixel_size : astropy.units.mas
+        The pixel size.
+    wavelength : astropy.units.um
 
     Returns
     -------
-    frequency_axis : numpy.ndarray
-        The frequency axis [u.m].
+    frequency_axis : astropy.units.m/astropy.units.rad
     """
-    return np.fft.fftshift(np.fft.fftfreq(dim, pixel_size))*wavelength
+    return np.fft.fftshift(
+        np.fft.fftfreq(dim, pixel_size.to(u.rad).value))*wavelength.to(u.m)
 
 
-def interpolate_for_coordinates(fourier_transform: np.ndarray,
-                                dim: float,
-                                pixel_size: float,
-                                ucoord: np.ndarray,
-                                vcoord: np.ndarray,
-                                wavelength: np.ndarray):
+# TODO: Look up how to calculate the closure phases from the fourier transform
+def interpolate_coordinates(fourier_transform: np.ndarray,
+                            dim: float,
+                            pixel_size: u.mas,
+                            ucoord: np.ndarray,
+                            vcoord: np.ndarray,
+                            wavelength: np.ndarray):
     """Interpolate the coordinates.
 
     Parameters
     ----------
     fourier_transform : numpy.ndarray
     dim : float
-    pixel_size : float
-    ucoord : numpy.ndarray
-    vcoord : numpy.ndarray
-    wavelength : numpy.ndarray
+    pixel_size : astropy.units.mas
+    ucoord : astropy.units.m
+    vcoord : astropy.units.m
+    wavelength : astropy.units.um
 
     Returns
     -------
     numpy.ndarray
     """
+    intp_setting = {"method": "linear",
+                    "fill_value": None, "bounds_error": True}
     frequency_axis = get_frequency_axis(dim, pixel_size, wavelength)
     grid = (frequency_axis, frequency_axis)
     coordinates = np.transpose([vcoord, ucoord])
-    real = interpn(grid, np.real(fourier_transform), coordinates)
-    imag = interpn(grid, np.imag(fourier_transform), coordinates)
-    return real+imag*1j
-
-# TODO: Look up how to calculate the closure phases from the fourier transform
+    real = interpn(
+        grid, np.real(fourier_transform), coordinates, **intp_setting)
+    imag = interpn(
+        grid, np.imag(fourier_transform), coordinates, **intp_setting)
+    return real+1j*imag
 
 
 if __name__ == "__main__":
-    print(get_frequency_axis(512, 0.1*u.mas.to(u.rad), 4.5e-6))
+    print(get_frequency_axis(512, 0.1*u.mas, 4.5*u.um))
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -2,11 +2,12 @@ import astropy.units as u
 import numpy as np
 import pytest
 
-from ppdmod.fft import compute_2Dfourier_transform, interpolate_for_coordinates
+from ppdmod.fft import compute_2Dfourier_transform, get_frequency_axis,\
+    interpolate_coordinates
 from ppdmod.utils import uniform_disk, uniform_disk_vis
 
-# TODO: Make tests for the phase that make pictures.
 
+# TODO: Make tests for the phase that make pictures.
 @pytest.fixture
 def ucoord() -> u.m:
     """Sets the ucoord."""
@@ -19,24 +20,100 @@ def vcoord(ucoord: u.m) -> u.m:
     return ucoord*0
 
 
+@pytest.fixture
+def u123coord() -> u.m:
+    """Sets the ucoords for the closure phases."""
+    coordinates = np.random.rand(3, 4)*100
+    sign = np.random.randint(2, size=(3, 4))
+    sign[sign == 0.] = -1
+    return coordinates*sign*u.m
+
+
+@pytest.fixture
+def v123coord() -> u.m:
+    """Sets the ucoords for the closure phases."""
+    coordinates = np.random.rand(3, 4)*100
+    sign = np.random.randint(2, size=(3, 4))
+    sign[sign == 0.] = -1
+    return coordinates*sign*u.m
+
+
+@pytest.fixture
+def pixel_size() -> u.mas:
+    """Sets the pixel size."""
+    return 0.1*u.mas
+
+
+@pytest.fixture
+def wavelength() -> u.um:
+    """Sets the wavelength."""
+    return (1.02322101e-05*u.m).to(u.um)
+
+
+def test_compute2Dfourier_transform(pixel_size: u.mas) -> None:
+    """Tests the computation of the 2D fourier transform."""
+    ud = uniform_disk(pixel_size, 512, diameter=4*u.mas)
+    ft = compute_2Dfourier_transform(ud)
+    assert ft.shape == ud.shape
+    assert ft.dtype == np.complex128
+
+
+# TODO: Test input for both wavelength in meter and um?
+# TODO: Check that the Parameter and such actually gets the right values (even with
+# the new scheme). Also the set data.
+def test_get_frequency_axis(pixel_size: u.mas, wavelength: u.um) -> None:
+    """Tests the frequency axis calculation and transformation."""
+    frequency_axis = get_frequency_axis(512, pixel_size, wavelength)
+    assert frequency_axis.unit == u.m
+    assert frequency_axis.shape == (512, )
+
+
 @pytest.mark.parametrize(
     "diameter, dim",
     [tuple([diameter, dim])
      for dim in [1024, 2048, 4096]
      for diameter in [4, 10, 20]*u.mas])
-def test_interpolation(diameter: u.mas, dim: float,
-                       ucoord: u.m, vcoord: u.m) -> None:
+def test_cphases_interpolation(diameter: u.mas, dim: float,
+                               u123coord: u.m, v123coord: u.m,
+                               pixel_size: u.mas, wavelength: u.um) -> None:
+    """Tests the interpolation of the closure phases."""
+    ft = compute_2Dfourier_transform(uniform_disk(pixel_size, dim,
+                                                  diameter=diameter))
+    interpolated_cphase = interpolate_coordinates(
+        ft, dim, pixel_size, u123coord, v123coord, wavelength)
+    interpolated_cphase = np.product(
+        interpolated_cphase/ft[dim//2, dim//2], axis=1)
+    interpolated_cphase = np.real(interpolated_cphase)
+
+    cphase = []
+    for ucoord, vcoord in zip(u123coord, v123coord):
+        tmp_cphase = uniform_disk_vis(diameter, ucoord, vcoord, wavelength)
+        cphase.append(tmp_cphase)
+    cphase = np.product(cphase, axis=0)
+    if dim == 1024:
+        assert np.allclose(cphase, interpolated_cphase, atol=1e-1)
+    else:
+        assert np.allclose(cphase, interpolated_cphase, atol=1e-2)
+
+
+@pytest.mark.parametrize(
+    "diameter, dim",
+    [tuple([diameter, dim])
+     for dim in [1024, 2048, 4096]
+     for diameter in [4, 10, 20]*u.mas])
+def test_vis_interpolation(diameter: u.mas, dim: float,
+                           ucoord: u.m, vcoord: u.m,
+                           pixel_size: u.mas, wavelength: u.um) -> None:
     """This tests the interpolation of the Fourier transform,
     but more importantly, implicitly the unit conversion of the
-    frequency axis."""
-    pixel_size, wavelength = 0.1*u.mas, 1.02322101e-05*u.m
+    frequency axis for the visibilitites/correlated fluxes."""
     ft = compute_2Dfourier_transform(uniform_disk(pixel_size, dim,
                                                   diameter=diameter))
     vis = uniform_disk_vis(diameter, ucoord, vcoord, wavelength)
-    interpolated_values = interpolate_for_coordinates(ft, dim,
-                                                      pixel_size.to(u.rad),
-                                                      ucoord, vcoord,
-                                                      wavelength)
+    interpolated_values = interpolate_coordinates(ft, dim,
+                                                  pixel_size,
+                                                  ucoord, vcoord,
+                                                  wavelength)
     interpolated_values /= ft[dim//2, dim//2]
     interpolated_values = np.real(interpolated_values)
     assert np.allclose(vis, interpolated_values, atol=1e-2)
