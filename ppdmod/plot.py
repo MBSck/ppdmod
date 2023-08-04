4543e0dea727f07bbd540cfd952f7c63981fc2ca
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-08-04 22:24:25 +0200

    Work on `plot`

--- ppdmod/plot.py
+++ ppdmod/plot.py
@@ -1,42 +1,48 @@
 from typing import Optional, List
+from pathlib import Path
 
-from astropy.units import Quantity
+import corner
+import matplotlib.pyplot as plt
+import numpy as np
+
+from .options import OPTIONS
 
 
 def plot_corner(sampler: np.ndarray,
-                labels: List, wavelength: float,
+                wavelength: float,
                 save_path: Optional[str] = "") -> None:
     """Plots the corner plot of the posterior spread"""
     samples = sampler.get_chain(flat=True)
-    fig = corner.corner(samples, show_titles=True, labels=labels,
-                       plot_datapoints=True, quantiles=[0.16, 0.5, 0.84])
-    plot_name = f"Corner_plot_{(wavelength):.2f}.png"
+    corner.corner(samples, show_titles=True,
+                  labels=OPTIONS["model.params"].keys(),
+                  plot_datapoints=True,
+                  quantiles=[0.16, 0.5, 0.84])
 
-    if save_path == "":
-        plt.savefig(plot_name)
+    if save_path:
+        plt.savefig(
+            Path(save_path) / f"Corner_plot_{(wavelength):.2f}.png")
     else:
-        plt.savefig(os.path.join(save_path, plot_name))
+        plt.show()
 
 
-def plot_chains(sampler: np.ndarray, theta: List,
-                labels: List, wavelength: float,
-                save_path: Optional[str] = "") -> None:
+def plot_chains(sampler: np.ndarray,
+                theta: List,
+                wavelength: float,
+                save_path: Optional[str] = None) -> None:
     """Plots the chains for debugging to see if and how they converge"""
-    fig, axes = plt.subplots(len(theta), figsize=(10, 7), sharex=True)
+    _, axes = plt.subplots(len(theta), figsize=(10, 7), sharex=True)
     samples = sampler.get_chain()
     ndim = len(theta)
 
-    for i in range(ndim):
-        ax = axes[i]
-        ax.plot(samples[:, :, i], "k", alpha=0.3)
-        ax.set_xlim(0, len(samples))
-        ax.set_ylabel(labels[i])
-        ax.yaxis.set_label_coords(-0.1, 0.5)
+    for index in range(ndim):
+        axes[index].plot(samples[:, :, index], "k", alpha=0.3)
+        axes[index].set_xlim(0, len(samples))
+        axes[index].set_ylabel(OPTIONS["model.params"].keys()[index])
+        axes[index].yaxis.set_label_coords(-0.1, 0.5)
+    axes[-1].set_xlabel("step number")
 
-    axes[-1].set_xlabel("step number");
-    plot_name = f"Chain_plot_{(wavelength):.2f}.png"
-
-    if save_path == "":
-        plt.savefig(plot_name)
+    if save_path:
+        plt.savefig(
+            Path(save_path) / f"Chain_plot_{(wavelength):.2f}.png")
     else:
-        plt.savefig(os.path.join(save_path, plot_name))
+        plt.show()
