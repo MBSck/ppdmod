ad41a760f2a7274c7a126b56a8b18c88ad678604
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-08-14 17:39:28 +0200

    Work on `plots`

--- ppdmod/plot.py
+++ ppdmod/plot.py
@@ -5,17 +5,28 @@ import corner
 import matplotlib.pyplot as plt
 import numpy as np
 
+from .mcmc import calculate_observables
 from .model import Model
+from .data import ReadoutFits
 from .options import OPTIONS
 
 
-def plot_corner(sampler: np.ndarray, model: Model,
+def plot_corner(sampler: np.ndarray, labels: List[str],
                 save_path: Optional[str] = None) -> None:
-    """Plots the corner plot of the posterior spread"""
+    """Plots the corner of the posterior spread.
+
+    Parameters
+    ----------
+    sampler : numpy.ndarray
+        The emcee sampler.
+    labels : list of str
+        The parameter labels.
+    save_path : str, optional
+        The save path. The default is None.
+    """
     samples = sampler.get_chain(flat=True)
     corner.corner(samples, show_titles=True,
-                  labels=list(model.params.keys()),
-                  plot_datapoints=True,
+                  labels=labels, plot_datapoints=True,
                   quantiles=[0.16, 0.5, 0.84])
 
     if save_path is not None:
@@ -25,17 +36,26 @@ def plot_corner(sampler: np.ndarray, model: Model,
         plt.show()
 
 
-def plot_chains(theta: List, sampler: np.ndarray,
+def plot_chains(sampler: np.ndarray, labels: List[str],
                 save_path: Optional[str] = None) -> None:
-    """Plots the chains for debugging to see if and how they converge"""
-    _, axes = plt.subplots(len(theta), figsize=(10, 7), sharex=True)
-    samples = sampler.get_chain()
-    ndim = len(theta)
+    """Plots the fitter's chains.
 
-    for index in range(ndim):
+    Parameters
+    ----------
+    sampler : numpy.ndarray
+        The emcee sampler.
+    labels : list of str
+        The parameter labels.
+    save_path : str, optional
+        The save path. The default is None.
+    """
+    _, axes = plt.subplots(len(labels), figsize=(10, 7), sharex=True)
+    samples = sampler.get_chain()
+
+    for index, label in enumerate(labels):
         axes[index].plot(samples[:, :, index], "k", alpha=0.3)
         axes[index].set_xlim(0, len(samples))
-        axes[index].set_ylabel(OPTIONS["model.params"].keys()[index])
+        axes[index].set_ylabel(label)
         axes[index].yaxis.set_label_coords(-0.1, 0.5)
     axes[-1].set_xlabel("step number")
 
@@ -46,61 +66,25 @@ def plot_chains(theta: List, sampler: np.ndarray,
 
 
 # TODO: Finish this.
-def plot_amp_phase_comparison(best_fit_total_fluxes,
-                              best_fit_corr_fluxes, best_fit_cphases,
-                              matplot_axes: Optional[List] = []) -> None:
-    """Plots the deviation of a model from real data of an object for both
-    amplitudes and phases (closure phases)
+def plot_observed_vs_model(
+        image: np.ndarray,
+        pixel_size: u.mas,
+        wavelength: u.um,
+        fits_files: List[ReadoutFits],
+        matplot_axes: Optional[List] = None,
+        data_to_plot: Optiona[List[str]] = OPTIONS["fit.data"]) -> None:
+    """Plots the deviation of a model from real data of an object for
+    total flux, visibilities and closure phases.
 
     Parameters
     ----------
-    amp_data: List
-        Contains both the model's and the real object's amplitude data and
-        errors in the following format [[real_obj, real_err], [model]
-    cphase_data: List
-        Contains both the model's and the real object's closure phase data and
-        errors in the following format [[real_obj, real_err], [model]]
-    baselines: List
-        The baselines of the amplitudes
-    t3phi_baselines: List
-        The baselines of the closure phases
-    matplot_axes: List, optional
-        The axes of matplotlib if this plot is to be embedded in an already
-        existing one
     """
-    if matplot_axes:
+    if matplot_axes is not None:
         ax, bx = matplot_axes
     else:
         fig, axarr = plt.subplots(1, 2, figsize=(10, 5))
         ax, bx = axarr.flatten()
 
-    if data.fit_total_flux:
-        max_flux, mod_max_flux = np.max(data.total_fluxes.value),\
-                                            np.max(best_fit_total_fluxes.value)
-        y_max_flux = max_flux if max_flux > mod_max_flux else mod_max_flux
-    else:
-        y_max_flux = np.max(best_fit_total_fluxes.value)
-
-    y_space_flux = np.sqrt(y_max_flux**2)*0.1
-    y_lim_flux = [0, y_max_flux+y_space_flux]
-
-    min_cphases, max_cphases = np.min(data.cphases.value), np.max(data.cphases.value)
-    min_mod_cphases, max_mod_cphases = np.min(best_fit_cphases.value),\
-        np.max(best_fit_cphases.value)
-    y_min_cphase = min_cphases if min_cphases < min_mod_cphases\
-        else min_mod_cphases
-    y_max_cphase = max_cphases if max_cphases > max_mod_cphases\
-        else max_mod_cphases
-    y_space_cphase = np.sqrt(y_max_cphase**2+y_min_cphase**2)*0.1
-    y_lim_cphase = [y_min_cphase-y_space_cphase, y_max_cphase+y_space_cphase]
-
-    # TODO: Add more colors
-    color_real_data = ["goldenrod", "darkgoldenrod", "gold"]
-    color_fit_data = ["midnightblue", "darkblue", "blue"]
-
-    # TODO: Think about how to get the positional angle and the
-    # axis ratio.
-    # TODO: Calculate the model here.
     total_fluxes, total_fluxes_err =\
         OPTIONS["data.total_flux"], OPTIONS["data.total_flux_error"]
     corr_fluxes, corr_fluxes_err =\
@@ -108,13 +92,19 @@ def plot_amp_phase_comparison(best_fit_total_fluxes,
     cphases, cphases_err =\
         OPTIONS["data.closure_phase"], OPTIONS["data.closure_phase_error"]
 
+    # TODO: Think about how to get the positional angle and the
+    # axis ratio.
+    # TODO: Calculate the model here.
     for index, (total_flux, total_flux_err, corr_flux,
                 corr_flux_err, cphase, cphase_err)\
             in enumerate(
                 zip(total_fluxes, total_fluxes_err, corr_fluxes,
                     corr_fluxes_err, cphases, cphases_err)):
         readout = OPTIONS["data.readouts"][index]
-        effective_baselines = calculate_effective_baselines(readout.ucoord,
+        total_flux_model, corr_flux_model, cphase_model =\
+            calculate_observables(image, pixel_size, wavelength)
+
+        effective_baselines = calculate_effective_baselines(
                                                             readout.vcoord,
                                                             axis_ratio,
                                                             pos_angle,
