38c2148950e90bd3d4905bdd550a47f0fc3eb078
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-09-04 14:22:10 +0200

    Update compiling

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
 import numpy as np
-from Cython.Build import cythonize
 from Cython.Compiler import Options
 
 Options.docstrings = True
 Options.annotate = True
+Options.language_level = 3
+Options.profile = False
 
+
+extensions = [Extension("_spectral_c", ["c_src/_spectral.c", "c_src/spectral.c"]),
+              Extension("_spectral_cy", ["cython_src/spectral.pyx"],)]
 
 setup(
     name="ppdmod",
-    ext_modules=cythonize("ppdmod/spectral.pyx",
-                          compiler_directives={"language_level": 3,
-                                               "profile": False}),
+    ext_modules=extensions,
     include_dirs=[np.get_include()]
 )
