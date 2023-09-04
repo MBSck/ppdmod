d5545e25ecb0f79a9025bd8623ed0d37a06532e0
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2023-09-04 14:30:42 +0200

    Switch to `setup.py`

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 
 import numpy as np
+import numpy.distutils.misc_util
 from Cython.Compiler import Options
 
 Options.docstrings = True
@@ -14,6 +15,39 @@ extensions = [Extension("_spectral_c", ["c_src/_spectral.c", "c_src/spectral.c"]
 
 setup(
     name="ppdmod",
+    version="0.9.0",
+    author="Marten Scheuck",
+    author_email="martenscheuck@gmail.com",
+    url="https://github.com/MBSck/ppdmod",
+    packages=["ppdmod"],
+    readme="README.md",
+    description="A package for modelling and model-fitting protoplanetary disks",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Framework :: Pytest",
+        "Framework :: Sphinx",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    include_dirs=[np.get_include()],
+    install_requires=[
+        'importlib-metadata; python_version<"3.8"',
+        "astropy >=5.1",
+        "corner >=2.2.2",
+        "emcee >=3.1.2",
+        "matplotlib >=3.5.3",
+        "numpy >=1.23.2",
+        "pip >=22.2.2",
+        "scipy >=1.9.1",
+        "tqdm >=4.64.0",
+        "pyfftw >=0.13.0",
+        "openpyxl >= 3.1.2",
+        "cython >= 3.0.2",
+    ],
     ext_modules=extensions,
-    include_dirs=[np.get_include()]
 )
