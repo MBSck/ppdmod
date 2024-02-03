Rename functions and fix wavelength calculation

ppdmod/component.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Tuple
+from typing import Optional, Tuple
 
 import astropy.units as u
 import numpy as np
@@ -70,7 +70,7 @@ class Component:
                 else:
                     self.params[key].value = value
 
-    def _calculate_internal_grid(
+    def calculate_internal_grid(
             self, dim: int, pixel_size: u.mas
             ) -> Tuple[u.Quantity[u.mas], u.Quantity[u.mas]]:
         """Calculates the model grid.
@@ -172,7 +172,7 @@ class AnalyticalComponent(Component):
             if pixel_size is None else pixel_size
         dim = u.Quantity(value=dim, unit=u.one, dtype=int)
         pixel_size = u.Quantity(value=pixel_size, unit=u.mas)
-        x_arr, y_arr = self._calculate_internal_grid(dim, pixel_size)
+        x_arr, y_arr = self.calculate_internal_grid(dim, pixel_size)
         return self._image_function(x_arr, y_arr, wavelength)
 
     def calculate_complex_visibility(
@@ -270,7 +270,7 @@ class HankelComponent(Component):
             self.params["eff_radius"](), self.params["dist"]())
         return self._stellar_angular_radius
 
-    def _calculate_internal_grid(self, dim: int) -> u.mas:
+    def calculate_internal_grid(self, dim: int) -> u.mas:
         """Calculates the model grid.
 
         Parameters
@@ -290,7 +290,7 @@ class HankelComponent(Component):
                                np.log10(rout.value), dim)*self.params["rin"].unit
         return radius.astype(OPTIONS.data.dtype.real)
 
-    def _get_opacity(self, wavelength: u.um) -> u.cm**2/u.g:
+    def get_opacity(self, wavelength: u.um) -> u.cm**2/u.g:
         """Set the opacity from wavelength."""
         if self.continuum_contribution:
             cont_weight = self.params["cont_weight"]()
@@ -298,9 +298,15 @@ class HankelComponent(Component):
                 + cont_weight*self.params["kappa_cont"](wavelength)
         else:
             opacity = self.params["kappa_abs"](wavelength)
-        return opacity.astype(OPTIONS.data.dtype.real)
 
-    def _azimuthal_modulation(self, xx: u.mas, yy: u.mas) -> u.one:
+        opacity = opacity.astype(OPTIONS.data.dtype.real)
+        if opacity.size == 1:
+            return opacity.squeeze()
+
+        shape = [np.newaxis for _ in range(len(wavelength.shape)-1)]
+        return opacity[:, *shape]
+
+    def calculate_azimuthal_modulation(self, xx: u.mas, yy: u.mas) -> u.one:
         """Calculates the azimuthal modulation."""
         if not self.asymmetric:
             return 1
@@ -308,25 +314,25 @@ class HankelComponent(Component):
                 * np.cos(np.arctan2(yy, xx)-self.params["phi"]()))
         return azimuthal_modulation.astype(OPTIONS.data.dtype.real)
 
-    def _temperature_profile_function(
+    def calculate_temperature(
             self, radius: u.mas, innermost_radius: u.mas) -> u.K:
         """Calculates a 1D-temperature profile."""
         if self.const_temperature:
-            temperature_profile = np.sqrt(self.stellar_radius_angular/(2.0*radius))\
+            temperature = np.sqrt(self.stellar_radius_angular/(2.0*radius))\
                     * self.params["eff_temp"]()
         else:
-            temperature_profile = self.params["inner_temp"]()\
+            temperature = self.params["inner_temp"]()\
                     * (radius/innermost_radius)**(-self.params["q"]())
-        return temperature_profile.astype(OPTIONS.data.dtype.real)
+        return temperature.astype(OPTIONS.data.dtype.real)
 
-    def _surface_density_profile_function(
+    def calculate_surface_density(
             self, radius: u.mas, innermost_radius: u.mas) -> u.one:
         """Calculates a 1D-surface density profile."""
         surface_density = self.params["inner_sigma"]()\
                 * (radius/innermost_radius)**(-self.params["p"]())
         return surface_density.astype(OPTIONS.data.dtype.real)
 
-    def _emissivity_profile_function(
+    def calculate_emissivity(
             self, radius: u.mas, innermost_radius: u.mas, wavelength: u.um) -> u.one:
         """Calculates a 1D-thickness profile."""
         if wavelength.shape == ():
@@ -334,13 +340,13 @@ class HankelComponent(Component):
 
         if self.optically_thick:
             return 1
-        surface_density_profile = self._surface_density_profile_function(
+        surface_density = self.calculate_surface_density(
             radius, innermost_radius)
-        optical_depth = surface_density_profile*self._get_opacity(wavelength)
-        thickness_profile = (1-np.exp(-optical_depth/self.params["elong"]()))
-        return thickness_profile.astype(OPTIONS.data.dtype.real)
+        optical_depth = surface_density*self.get_opacity(wavelength)
+        emissivity = (1-np.exp(-optical_depth/self.params["elong"]()))
+        return emissivity.astype(OPTIONS.data.dtype.real)
 
-    def _brightness_profile_function(self, radius: u.mas, wavelength: u.um) -> u.Jy:
+    def calculate_brightness(self, radius: u.mas, wavelength: u.um) -> u.Jy:
         """Calculates a 1D-brightness profile from a dust-surface density- and
         temperature profile.
 
@@ -359,13 +365,13 @@ class HankelComponent(Component):
         innermost_radius = self.params["rin0"]()\
             if self.params["rin0"]() != 0 else self.params["rin"]()
 
-        temperature_profile = self._temperature_profile_function(
+        temperature_profile = self.calculate_temperature(
                 radius, innermost_radius)
-        brightness_profile = BlackBody(temperature_profile)(wavelength)
-        emissivity_profile = self._emissivity_profile_function(
+        brightness = BlackBody(temperature_profile)(wavelength)
+        emissivity = self.calculate_emissivity(
                 radius, innermost_radius, wavelength)
-        brightness_profile *= emissivity_profile
-        return brightness_profile.astype(OPTIONS.data.dtype.real)
+        brightness *= emissivity
+        return brightness.astype(OPTIONS.data.dtype.real)
 
     # TODO: Check if the image is displayed properly
     def calculate_image(self, dim: int, pixel_size: u.mas, wavelength: u.um) -> u.Jy:
@@ -378,11 +384,11 @@ class HankelComponent(Component):
             pa, elong = self.params["pa"](), self.params["elong"]()
             xx = xx*np.cos(pa)+yy*np.sin(pa)
             yy = (-xx*np.sin(pa)+yy*np.cos(pa))/elong
-        azimuthal_modulation = self._azimuthal_modulation(xx, yy)
+        azimuthal_modulation = self.calculate_azimuthal_modulation(xx, yy)
         radius = np.hypot(xx, yy)
         radial_profile = np.logical_and(radius >= self.params["rin"](),
                                         radius <= self.params["rout"]())
-        brightness_profile = self._brightness_profile_function(
+        brightness_profile = self.calculate_brightness(
                 radius, wavelength).to(u.erg/(u.cm**2*u.rad**2*u.s*u.Hz))\
             * pixel_size.to(u.rad)**2
         image = brightness_profile.to(u.Jy)*radial_profile*azimuthal_modulation
@@ -447,11 +453,16 @@ class HankelComponent(Component):
             The visibilities.
         modulations : astropy.units.Jy
         """
-        radius, axis_ratio = radius.to(u.rad), self.params["elong"]()
+        axis_ratio = self.params["elong"]()
         baselines, baseline_angles = calculate_effective_baselines(
                 ucoord, vcoord, axis_ratio, self.params["pa"]())
 
-        wavelength = wavelength[:, np.newaxis].to(u.m)
+        wavelength = wavelength[:, np.newaxis]
+        brightness = self.calculate_brightness(radius, wavelength)
+        radius = radius.to(u.rad)
+
+        wavelength = wavelength.to(u.m)
+        brightness = brightness[:, np.newaxis, :]
         if len(ucoord.shape) == 1:
             baselines = (baselines/wavelength).value*u.rad
             baselines = baselines[..., np.newaxis]
@@ -461,24 +472,21 @@ class HankelComponent(Component):
             baselines = (baselines[np.newaxis, ...]/wavelength).value*u.rad
             baselines = baselines[..., np.newaxis]
             baseline_angles = baseline_angles[np.newaxis, ..., np.newaxis]
+            brightness = brightness[..., np.newaxis, :]
 
-        brightness_profile = self._brightness_profile_function(
-                radius, wavelength[:, np.newaxis, :])
-
-        visibility = 2*np.pi*np.trapz(radius*brightness_profile*j0(
+        visibility = 2*np.pi*np.trapz(radius*brightness*j0(
             2.*np.pi*radius.value*baselines.value), radius).to(u.Jy)
         visibility = visibility.astype(OPTIONS.data.dtype.complex)
 
         modulation = self.hankel_modulation(
-                radius, brightness_profile,
-                baselines, baseline_angles)
+                radius, brightness, baselines, baseline_angles)
         return visibility.astype(OPTIONS.data.dtype.complex), \
             modulation.astype(OPTIONS.data.dtype.complex)
 
     def calculate_flux(self, wavelength: u.um) -> u.Jy:
         """Calculates the total flux from the hankel transformation."""
-        radius = self._calculate_internal_grid(self.params["dim"]())
-        brightness_profile = self._brightness_profile_function(
+        radius = self.calculate_internal_grid(self.params["dim"]())
+        brightness_profile = self.calculate_brightness(
                 radius, wavelength[:, np.newaxis])
         flux = (2.*np.pi*np.trapz(radius*brightness_profile, radius).to(u.Jy)).value
         return np.abs(flux.reshape((flux.shape[0], 1)).astype(OPTIONS.data.dtype.real))
@@ -486,7 +494,7 @@ class HankelComponent(Component):
     def calculate_visibility(self, ucoord: u.m, vcoord: u.m,
                              wavelength: u.um, **kwargs) -> np.ndarray:
         """Calculates the correlated fluxes via the hankel transformation."""
-        radius = self._calculate_internal_grid(self.params["dim"]())
+        radius = self.calculate_internal_grid(self.params["dim"]())
         vis, vis_mod = self.hankel_transform(
                 radius, ucoord, vcoord, wavelength, **kwargs)
         if vis_mod.size != 0:
@@ -496,7 +504,7 @@ class HankelComponent(Component):
     def calculate_closure_phase(self, ucoord: u.m, vcoord: u.m,
                                 wavelength: u.um, **kwargs) -> np.ndarray:
         """Calculates the closure phases via hankel transformation."""
-        radius = self._calculate_internal_grid(self.params["dim"]())
+        radius = self.calculate_internal_grid(self.params["dim"]())
         vis, vis_mod = self.hankel_transform(
                 radius, ucoord, vcoord, wavelength, **kwargs)
         if vis_mod.size != 0:

ppdmod/custom_components.py
@@ -70,17 +70,17 @@ class Star(AnalyticalComponent):
 
     def calculate_flux(self, wavelength: u.um) -> u.Jy:
         """Calculates the flux of the star."""
-        wavelength = wavelength[:, np.newaxis]
         if self.params["f"].value is not None:
-            stellar_flux = self.params["f"](wavelength).astype
+            stellar_flux = self.params["f"](wavelength)
         else:
             plancks_law = models.BlackBody(
                     temperature=self.params["eff_temp"]())
-            spectral_radiance = plancks_law(wavelength.to(u.m)).to(
+            spectral_radiance = plancks_law(wavelength).to(
                 u.erg/(u.cm**2*u.Hz*u.s*u.rad**2))
             stellar_flux = np.pi*(spectral_radiance
                                   * self.stellar_radius_angular**2).to(u.Jy)
-        return stellar_flux.astype(OPTIONS.data.dtype.real)
+        stellar_flux = stellar_flux.astype(OPTIONS.data.dtype.real)
+        return stellar_flux.reshape((wavelength.size, 1))
 
     def _image_function(self, xx: u.mas, yy: u.mas,
                         wavelength: Optional[u.Quantity[u.m]] = None,

ppdmod/fitting.py
@@ -125,14 +125,15 @@ def calculate_chi_sq(data: u.quantity, error: u.quantity,
     return -0.5*np.sum(diff**2*inv_sigma_squared + np.log(1/inv_sigma_squared))
 
 
-def calculate_observables(components: List[Component]):
+def calculate_observables(components: List[Component],
+                          wavelength: Optional[np.ndarray] = None):
     """Calculates the observables from the model."""
+    wavelength = OPTIONS.fit.wavelengths if wavelength is None else wavelength
     corr_flux = "vis2" not in OPTIONS.fit.data
     vis = OPTIONS.data.vis if corr_flux else OPTIONS.data.vis2
     ucoord, vcoord = vis.ucoord, vis.vcoord
     u123coord = OPTIONS.data.t3.u123coord
     v123coord = OPTIONS.data.t3.v123coord
-    wavelength = OPTIONS.fit.wavelengths
 
     flux_model, vis_model, t3_model = None, None, None
     if components is not None:

ppdmod/options.py
@@ -112,7 +112,7 @@ vis2 = SimpleNamespace(value=np.array([]), err=np.array([]),
 t3 = SimpleNamespace(value=np.array([]), err=np.array([]),
                      u123coord=np.array([]), v123coord=np.array([]))
 flux = SimpleNamespace(value=np.array([]), err=np.array([]))
-binning = SimpleNamespace(window=0.1)
+binning = SimpleNamespace(window=0.1*u.um)
 gravity = SimpleNamespace(index=20)
 dtype = SimpleNamespace(complex=np.complex64, real=np.float32)
 data = SimpleNamespace(readouts=[], flux=flux, vis=vis,

ppdmod/plot.py
@@ -558,9 +558,11 @@ def plot_overview(data_to_plot: Optional[List[str]] = None,
         if wavelength_range is not None:
             if not (wavelength_range[0] <= wavelength <= wavelength_range[1]):
                 continue
-        effective_baselines = np.hypot(vis.ucoord, vis.vcoord)*u.m
-        longest_baselines = np.hypot(
-                t3.u123coord, t3.v123coord).max(axis=0)*u.m
+
+        effective_baselines, _ = calculate_effective_baselines(
+                vis.ucoord, vis.vcoord)
+        longest_baselines, _ = calculate_effective_baselines(
+                t3.u123coord, t3.v123coord, longest=True)
 
         effective_baselines_mlambda = effective_baselines/wavelength.value
         longest_baselines_mlambda = longest_baselines/wavelength.value
@@ -650,9 +652,10 @@ def plot_overview(data_to_plot: Optional[List[str]] = None,
         if key == "t3":
             ax.set_xlabel(r"$\mathrm{B}_{\mathrm{max}}$ (M$\lambda$)")
             ax.set_ylabel(r"Closure Phases ($^\circ$)")
-            lower_bound = np.min([np.min(value) for value in t3.value])
+            nan_t3 = np.isnan(t3.value)
+            lower_bound = t3.value[~nan_t3].min()
             lower_bound += lower_bound*0.25
-            upper_bound = np.max([np.max(value) for value in t3.value])
+            upper_bound = t3.value[~nan_t3].max()
             upper_bound += upper_bound*0.25
             if "t3" in ylimits:
                 ax.set_ylim(ylimits["t3"])
@@ -753,8 +756,6 @@ def plot_target(target: str,
 def plot_observables(target: str,
                      wavelength_range: u.um,
                      components: List[Component],
-                     fits_files: List[Path],
-                     corr_flux: Optional[bool] = None,
                      save_dir: Optional[Path] = None) -> None:
     """Plots the observables of the model.
 
@@ -763,54 +764,21 @@ def plot_observables(target: str,
     wavelength_range : astropy.units.m
     """
     save_dir = Path.cwd() if save_dir is None else save_dir
-    wavelengths = np.linspace(wavelength_range[0],
-                              wavelength_range[1])
+    baseline_dir = save_dir / "baselines"
+    baseline_dir.mkdir(exist_ok=True, parents=True)
+    vis_dir = baseline_dir / "vis"
+    vis_dir.mkdir(exist_ok=True, parents=True)
+    t3_dir = baseline_dir / "t3"
+    t3_dir.mkdir(exist_ok=True, parents=True)
 
-    readouts = list(map(ReadoutFits, fits_files))
-    ucoord = np.concatenate([readout.ucoord for readout in readouts])
-    vcoord = np.concatenate([readout.vcoord for readout in readouts])
-    u123coord = np.hstack([readout.u123coord for readout in readouts])
-    v123coord = np.hstack([readout.v123coord for readout in readouts])
-
-    corr_flux = not "vis2" in OPTIONS.fit.data if corr_flux is None\
-        else corr_flux
-
-    flux = []
-    vis = np.empty([len(wavelengths), ucoord.shape[0]])
-    cphase = np.empty([len(wavelengths), u123coord.shape[1]])
-    for index, wavelength in enumerate(wavelengths):
-        stellar_flux = components[0].calculate_stellar_flux(wavelength).value
-        tmp_flux, tmp_vis, tmp_cphase = 0, None, None
-        for component in components[1:]:
-            tmp_flux += component.calculate_flux(wavelength)
-
-            if tmp_vis is None:
-                tmp_vis = component.calculate_corr_flux(
-                        ucoord, vcoord, wavelength).tolist()
-                tmp_cphase = component.calculate_closure_phase(
-                        u123coord, v123coord, wavelength).tolist()
-            else:
-                tmp_vis.extend(component.calculate_corr_flux(
-                        ucoord, vcoord, wavelength).tolist())
-                tmp_cphase.extend(component.calculate_closure_phase(
-                        u123coord, v123coord, wavelength).tolist())
-
-        tmp_flux += stellar_flux
-        tmp_vis += stellar_flux
-
-        if not corr_flux:
-            tmp_vis /= tmp_flux
-
-        flux.append(tmp_flux)
-        vis[index] = tmp_vis
-        cphase[index] = tmp_cphase
-    flux = np.array(flux)
+    wavelength = np.linspace(wavelength_range[0], wavelength_range[1])
+    flux, vis, t3 = calculate_observables(components, wavelength=wavelength)
 
     _ = plt.figure(facecolor=OPTIONS.plot.color.background,
                    tight_layout=True)
     ax = plt.axes(facecolor=OPTIONS.plot.color.background)
     set_axes_color(ax, OPTIONS.plot.color.background)
-    ax.plot(wavelengths, flux, label="Model")
+    ax.plot(wavelength, flux, label="Model")
     plot_target(target, wavelength_range=wavelength_range, ax=ax)
     ax.set_xlabel(r"$\lambda$ ($\mu$m)")
     ax.set_ylabel("Flux (Jy)")
@@ -819,12 +787,6 @@ def plot_observables(target: str,
     plt.savefig(save_dir / "sed.pdf", format="pdf")
     plt.close()
 
-    baseline_dir = save_dir / "baselines"
-    baseline_dir.mkdir(exist_ok=True, parents=True)
-    vis_dir = baseline_dir / "vis"
-    vis_dir.mkdir(exist_ok=True, parents=True)
-    cphase_dir = baseline_dir / "cphase"
-    cphase_dir.mkdir(exist_ok=True, parents=True)
     for index, (uc, vc) in enumerate(zip(ucoord, vcoord)):
         _ = plt.figure(facecolor=OPTIONS.plot.color.background,
                        tight_layout=True)
@@ -832,7 +794,7 @@ def plot_observables(target: str,
         set_axes_color(ax, OPTIONS.plot.color.background)
 
         baseline, baseline_angle = np.hypot(uc, vc), np.arctan2(uc, vc)*u.rad.to(u.deg)
-        ax.plot(wavelengths, vis[:, index],
+        ax.plot(wavelengths[:, np.newaxis, np.newaxis], vis[:, index],
                 label=rf"B={baseline:.2f} m, $\phi$={baseline_angle:.2f}$^\circ$")
         ax.set_xlabel(r"$\lambda$ ($\mu$m)")
         ax.set_ylabel("Visibilities (Normalized)")
@@ -846,9 +808,9 @@ def plot_observables(target: str,
                        tight_layout=True)
         ax = plt.axes(facecolor=OPTIONS.plot.color.background)
         set_axes_color(ax, OPTIONS.plot.color.background)
-        ax.plot(wavelengths, cphase[:, index], label=f"B={baseline:.2f} m")
+        ax.plot(wavelengths, t3_model[:, index], label=f"B={baseline:.2f} m")
         ax.set_xlabel(r"$\lambda$ ($\mu$m)")
         ax.set_ylabel(r"Closure Phases ($^\circ$)")
         plt.legend()
-        plt.savefig(cphase_dir / f"t3_{baseline:.2f}.pdf", format="pdf")
+        plt.savefig(t3_dir / f"t3_{baseline:.2f}.pdf", format="pdf")
         plt.close()

ppdmod/utils.py
@@ -125,8 +125,10 @@ def distance_to_angular(diameter: u.mas, distance: u.pc) -> u.m:
 
 
 def calculate_effective_baselines(
-        ucoord: u.m, vcoord: u.m, axis_ratio: u.one,
-        pos_angle: u.deg, longest: Optional[bool] = False
+        ucoord: u.m, vcoord: u.m,
+        axis_ratio: Optional[u.one] = None,
+        pos_angle: Optional[u.deg] = None,
+        longest: Optional[bool] = False
         ) -> Tuple[u.Quantity[u.m], u.Quantity[u.one]]:
     """Calculates the effective baselines from the projected baselines
     in mega lambda.
@@ -141,6 +143,8 @@ def calculate_effective_baselines(
         The axis ratio of the ellipse
     pos_angle: astropy.units.deg
         The positional angle of the object
+    longest : bool, optional
+        If True, the longest baselines are returned.
 
     Returns
     -------
@@ -149,17 +153,19 @@ def calculate_effective_baselines(
     baselines_angles : astropy.units.rad
         Returns the effective baseline angles.
     """
-    if not isinstance(ucoord, u.Quantity):
-        ucoord, vcoord = map(lambda x: x*u.m, [ucoord, vcoord])
-    axis_ratio = axis_ratio*u.one\
-        if not isinstance(axis_ratio, u.Quantity) else axis_ratio
-    pos_angle = pos_angle*u.deg\
-        if not isinstance(pos_angle, u.Quantity) else pos_angle
+    ucoord, vcoord = map(lambda x: u.Quantity(x, u.m), [ucoord, vcoord])
+    if axis_ratio is not None and pos_angle is not None:
+        axis_ratio = u.Quantity(axis_ratio, u.one)
+        pos_angle = u.Quantity(pos_angle, u.deg)
 
-    ucoord_eff = ucoord*np.cos(pos_angle) - vcoord*np.sin(pos_angle)
-    vcoord_eff = ucoord*np.sin(pos_angle) + vcoord*np.cos(pos_angle)
-    baselines_eff = np.hypot(ucoord_eff, vcoord_eff*axis_ratio)
-    baseline_angles_eff = np.arctan2(ucoord_eff, vcoord_eff*axis_ratio)
+        ucoord_eff = ucoord*np.cos(pos_angle) - vcoord*np.sin(pos_angle)
+        vcoord_eff = (ucoord*np.sin(pos_angle) + vcoord*np.cos(pos_angle))
+        vcoord_eff *= axis_ratio
+    else:
+        ucoord_eff, vcoord_eff = ucoord, vcoord
+
+    baselines_eff = np.hypot(ucoord_eff, vcoord_eff)
+    baseline_angles_eff = np.arctan2(ucoord_eff, vcoord_eff)
 
     if longest:
         indices = baselines_eff.argmax(0)

tests/test_component.py
@@ -85,7 +85,7 @@ def test_eval(component: Component) -> None:
 def test_radius_calculation(component: Component) -> None:
     """Tests if the radius calculated from the grid works."""
     dim, pixel_size = 512, 0.1*u.mas
-    grid = component._calculate_internal_grid(dim, pixel_size)
+    grid = component.calculate_internal_grid(dim, pixel_size)
     plt.imshow(np.hypot(*grid))
     plt.title("Image space")
     plt.xlabel("dim [px]")
@@ -106,7 +106,7 @@ def test_radius_calculation(component: Component) -> None:
     assert elliptical_component.params["pa"]() == 33*u.deg
     assert elliptical_component.params["elong"]() == 0.6*u.one
 
-    grid = elliptical_component._calculate_internal_grid(dim, pixel_size)
+    grid = elliptical_component.calculate_internal_grid(dim, pixel_size)
     plt.imshow(np.hypot(*grid))
     plt.title("Image space")
     plt.xlabel("dim [px]")
@@ -178,7 +178,7 @@ def test_hankel_component_calculate_grid(
         hankel_component: HankelComponent, grid_type: str) -> None:
     """Tests the hankel component's grid calculation."""
     OPTIONS.model.gridtype = grid_type
-    radius = hankel_component._calculate_internal_grid(512)
+    radius = hankel_component.calculate_internal_grid(512)
     assert radius.unit == u.mas
     assert radius.shape == (512, )
     assert radius[0].value == hankel_component.params["rin"].value\
@@ -202,7 +202,7 @@ def test_hankel_component_hankel_transform(
         hankel_component: HankelComponent,
         order: int, wavelength: u.um) -> None:
     """Tests the hankel component's hankel transformation."""
-    radius = hankel_component._calculate_internal_grid(512)
+    radius = hankel_component.calculate_internal_grid(512)
 
     OPTIONS.model.modulation = order
 
@@ -245,7 +245,6 @@ def test_hankel_component_closure_phases(
     assert t3.shape == (wavelength.size, 4)
 
     OPTIONS.model.modulation = 0
-
 
 # TODO: Extend this test to account for multiple files (make files an input)
 @pytest.mark.parametrize(