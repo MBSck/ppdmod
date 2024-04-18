a961f8f0838faa171a9ac2e4c1103431c9289508
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-04-18 09:45:57 +0200

    Change ranges in `options`

--- ppdmod/component.py
+++ ppdmod/component.py
@@ -186,7 +186,6 @@ class Component:
             polar_angle = np.arctan2(yr, xr)
             image *= 1 + c*np.cos(polar_angle) + s*np.sin(polar_angle)
 
-        image /= image.max()
         return self.fr()*image.astype(OPTIONS.data.dtype.real)
 
 
