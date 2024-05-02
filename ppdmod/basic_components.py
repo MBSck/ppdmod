Fix computation of azimuthal modulation

ppdmod/basic_components.py
@@ -227,12 +227,8 @@ class Ring(Component):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.rin = Parameter(**STANDARD_PARAMETERS.rin)
-
-        if not self.thin:
-            if self.has_outer_radius:
-                self.rout = Parameter(**STANDARD_PARAMETERS.rout)
-            else:
-                self.width = Parameter(**STANDARD_PARAMETERS.width)
+        self.rout = Parameter(**STANDARD_PARAMETERS.rout)
+        self.width = Parameter(**STANDARD_PARAMETERS.width)
 
         self.eval(**kwargs)
 
@@ -271,23 +267,27 @@ class Ring(Component):
         brightness : astropy.unit.mas
             The radial brightness distribution
         """
-        brightness = kwargs.pop("brightness", None)
-        angle_diff = baseline_angles - (self.phi()-90*u.deg).to(u.rad)
+        phi = (self.phi()-180*u.deg).to(u.rad)
+        angle_diff = np.angle(np.exp(1j*(baseline_angles - phi).value))
+        
         if self.thin:
             xx = 2 * np.pi * self.rin().to(u.rad) * baselines
             vis = j0(xx).astype(complex)
-            vis += -1j * self.a() * np.cos(angle_diff) * j1(xx) if self.asymmetric else 0
+            if self.asymmetric:
+                vis += -1j * self.a() * np.cos(angle_diff) * j1(xx)
         else:
+            brightness = kwargs.pop("brightness", None)
             radius = self.compute_internal_grid().to(u.rad)
             xx = 2 * np.pi * radius * baselines
-            vis = (np.trapz(j0(xx), radius)).astype(complex)
-            vis += -1j * self.a() * np.cos(angle_diff) * np.trapz(j1(xx), radius) if self.asymmetric else 0
+            vis = np.trapz(j0(xx), radius).astype(complex)
+            if self.asymmetric:
+                vis += -1j * self.a() * np.cos(angle_diff) * np.trapz(j1(xx), radius)
 
             if brightness is None:
                 if self.has_outer_radius:
-                    vis /= self.rout() - self.rin()
+                    vis /= (self.rout() - self.rin()).to(u.rad)
                 else:
-                    vis /= self.width()
+                    vis /= self.width().to(u.rad)
 
             vis = vis[..., np.newaxis]
         return vis.value.astype(OPTIONS.data.dtype.complex)
@@ -322,7 +322,7 @@ class Ring(Component):
 
         if self.asymmetric:
             polar_angle = np.arctan2(yy, xx)
-            phi = (self.phi()-90*u.deg).to(u.rad)
+            phi = (self.phi()-180*u.deg).to(u.rad)
             c, s = self.a() * np.cos(phi), self.a() * np.sin(phi)
             image *= 1 + c * np.cos(polar_angle) + s * np.sin(polar_angle)
 
@@ -911,11 +911,10 @@ class StarHaloGauss(Component):
         if len(fs.shape) >= 1:
             fs = fs[..., np.newaxis]
             vis_star = fs
-            
-        fh = 1 - (fs + self.fc())
         if len(baselines.shape) == 4:
             fs = fs[..., np.newaxis]
             
+        fh = 1 - (fs + self.fc())
         divisor = (fh + fs) * wavelength_ratio**ks \
             + self.fc() * wavelength_ratio ** self.kc()
 

ppdmod/utils.py
@@ -67,7 +67,6 @@ def get_indices(values, array: np.ndarray,
     return indices
 
 
-# TODO: Replace with np.gradient, does the same thing
 def compute_photometric_slope(
         wavelengths: u.um, temperature: u.K) -> np.ndarray:
     """Computes the photometric slope of the data from
@@ -88,12 +87,7 @@ def compute_photometric_slope(
     wavelengths = u.Quantity(wavelengths, u.um)
     nu = (const.c/wavelengths.to(u.m)).to(u.Hz)
     blackbody = BlackBody(temperature)
-
-    delta_nu = (nu * 1e-5)
-    bb_upper, bb_lower = map(lambda x: blackbody(x), (nu+delta_nu, nu-delta_nu))
-    bb_diff = (bb_upper-bb_lower) / (2 * delta_nu)
-    photometric_slope = (nu / blackbody(nu)) * bb_diff
-    return photometric_slope.value
+    return np.gradient(np.log(blackbody(nu).value), np.log(nu.value))
 
 
 def compute_stellar_radius(luminosity: u.Lsun, temperature: u.K) -> u.Rsun:

tests/fitting/pionier_fscma.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-import astropy.units as u
+import numpy as np
 from ppdmod.basic_components import Ring, StarHaloRing
 from ppdmod.data import set_data, get_all_wavelengths
 from ppdmod.parameter import Parameter
@@ -22,15 +22,16 @@ if __name__ == "__main__":
     ks.free = False
     
     result_dir = Path("results/pionier")
-    ring = Ring(rin=2, asymmetric=True, a=1, phi=0)
-    image = ring.compute_image(512, 0.02, 1.68)
+    ring = Ring(rin=2, asymmetric=True, a=1, phi=30)
 
+    # FSCMa
     # shlr = StarHaloRing(
     #     fs=0.42, fc=0.55, flor=1.0,
     #     la=0.98, lkr=-0.26,
     #     ks=ks, kc=-4.12,
-    #     inc=0.63, pa=1.2*u.rad.to(u.deg),
+    #     inc=0.63, pa=1.2/np.pi*180,
     #     a=0.996393496566492,
     #     phi=100.40771131249006)
 
+    plot_components(ring, 512, 0.02, 3.5, savefig=result_dir / "test.png", save_as_fits=False)
     plot_components(ring, 512, 0.02, 3.5, savefig=result_dir / "test.fits", save_as_fits=True)