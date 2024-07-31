b98452e17557243e3646e0114abe9ba09d5dff24
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-07-31 14:04:06 +0200

    Update fit with more N band points

--- examples/hd142527.py
+++ examples/hd142527.py
@@ -38,16 +38,13 @@ def ptform_radii(theta: List[float]) -> np.ndarray:
 
 
 DATA_DIR = Path("../tests/data")
-wavelengths = {"hband": [1.7]*u.um,
-               "kband": [2.25]*u.um,
-               "lband": [3.2]*u.um,
-               "mband": [4.7]*u.um,
-               "nband": [8., 9., 10., 11.3, 12.5]*u.um}
+wavelengths = {"hband": [1.7]*u.um, "kband": [2.25]*u.um,
+               "lband": [3.2]*u.um, "mband": [4.7]*u.um,
+               "nband": [8., 8.5, 9., 9.5, 10., 11.3, 12.5]*u.um}
 
 OPTIONS.model.output = "non-normed"
 fits_files = list((DATA_DIR / "fits" / "hd142527").glob("*fits"))
-wavelength = np.concatenate((wavelengths["hband"],
-                             wavelengths["lband"],
+wavelength = np.concatenate((wavelengths["hband"], wavelengths["lband"],
                              wavelengths["mband"], wavelengths["nband"]))
 # wavelength = wavelengths["lband"]
 data = set_data(fits_files, wavelengths=wavelength, fit_data=["flux", "vis"])
