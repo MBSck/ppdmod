9bc82ffe79d99e50766bc9168c307562e9a63738
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2024-11-07 17:30:56 +0100

    Remove debug

--- examples/disc_fit.py
+++ examples/disc_fit.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     ncores = 70
     fit_params = {"nlive_init": 2000, "ptform": ptform}
     sampler = run_fit(
-        **fit_params, ncores=ncores, method="dynamic", save_dir=result_dir, debug=True
+        **fit_params, ncores=ncores, method="dynamic", save_dir=result_dir, debug=False
     )
 
     theta, uncertainties = get_best_fit(sampler, **fit_params)
