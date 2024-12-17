Correct effective baseline calculation to `np.arctan2(vcoord, ucoord)`
Also fix the tests connected to it

ppdmod/utils.py
@@ -244,7 +244,6 @@ def distance_to_angular(diameter: u.au, distance: u.pc) -> u.mas:
     return ((diameter.to(u.m) / distance.to(u.m)) * u.rad).to(u.mas)
 
 
-# TODO: I think the return_zero here can be removed
 def compute_effective_baselines(
     ucoord: u.m,
     vcoord: u.m,
@@ -292,7 +291,7 @@ def compute_effective_baselines(
         ucoord_eff *= inclination
 
     baselines_eff = np.hypot(ucoord_eff, vcoord_eff)
-    baseline_angles_eff = np.arctan2(ucoord_eff, vcoord_eff)
+    baseline_angles_eff = np.arctan2(vcoord_eff, ucoord_eff)
 
     if longest:
         indices = baselines_eff.argmax(0)