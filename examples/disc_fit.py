a0ca351066bc657b6e3f79d52f20613410eb277d
Marten B. Scheuck <58522505+MBSck@users.noreply.github.com>
2025-01-22 17:24:09 +0100

    Update fit

--- examples/disc_fit.py
+++ examples/disc_fit.py
@@ -155,7 +155,7 @@ ndim = len(LABELS)
 
 if __name__ == "__main__":
     ncores = 70
-    fit_params = {"dlogz_init": 0.5, "nlive_init": 2000, "nlive_batch": 200, "ptform": ptform}
+    fit_params = {"dlogz_init": 0.01, "nlive_init": 2000, "nlive_batch": 200, "ptform": ptform}
     sampler = run_fit(**fit_params, ncores=ncores, save_dir=result_dir, debug=False)
 
     theta, uncertainties = get_best_fit(sampler)
