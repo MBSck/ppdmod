Fix averaging bug

ppdmod/data.py
@@ -297,7 +297,6 @@ def average_data() -> None:
     flux, flux_err = OPTIONS.data.flux.value, OPTIONS.data.flux.err
     flux_averaged = np.ma.average(flux, weights=1 / flux_err**2, axis=-1)
     flux_err_averaged = np.ma.sqrt(1 / np.ma.sum(flux_err, axis=-1) ** 2)
-    # TODO: Is this really needed?
     ind = np.where(flux_err_averaged < flux_averaged * 0.05)
     flux_err_averaged[ind] = flux_averaged[ind] * 0.05
     flux_ratio = flux / flux_averaged[:, np.newaxis]
@@ -316,14 +315,18 @@ def average_data() -> None:
             ind = np.where((limits[0] < wavelengths) & (limits[1] > wavelengths))[0]
 
         band_ratio = flux_ratio[:, index][ind]
-        if np.all(band_ratio.mask):
-            flux_ratio[:, index][ind] = 1.
-        elif np.all(~band_ratio.mask):
+        if band_ratio.compressed().size == band_ratio.size:
             continue
+
+        if band_ratio.compressed().size == 0:
+            flux_ratio[:, index][ind] = 1.
+        elif band_ratio.compressed().size == 1:
+            flux_ratio[:, index][ind] = band_ratio.compressed()
         else:
             interp_ratios = interp1d(wavelengths[ind][~band_ratio.mask],
                                     band_ratio.compressed(), fill_value="extrapolate")
             flux_ratio[:, index][ind] = interp_ratios(wavelengths[ind])
+
 
     for key in ["vis", "vis2"]:
         value = getattr(OPTIONS.data, key).value