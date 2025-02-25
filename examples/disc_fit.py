68941087d968d39e8507011b00eba8801fa83bac
M. B. Scheuck <programming.mbsck@mailbox.org>
2025-02-25 10:51:19 +0100

    Remove asymmetric parameters from outer zone

--- examples/disc_fit.py
+++ examples/disc_fit.py
@@ -144,8 +144,8 @@ second = TempGrad(
     rin=rin2,
     rout=rout2,
     p=p2,
-    sigma0=sigma02,
-    rho1=rho21,
+    # sigma0=sigma02,
+    # rho1=rho21,
     theta1=theta21,
     **shared_params,
 )
