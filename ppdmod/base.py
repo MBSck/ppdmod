Fix image calculation

ppdmod/base.py
@@ -196,7 +196,7 @@ class FourierComponent(Component):
     def compute_image(self, dim: int, pixel_size: u.mas, wl: u.um) -> np.ndarray:
         """Computes the image."""
         wl = wl[np.newaxis, np.newaxis]
-        xx = np.linspace(-0.5, 0.5, dim) * pixel_size * dim
+        xx = np.linspace(-0.5, 0.5, dim, endpoint=False) * pixel_size * dim
         xxt, yyt = transform_coordinates(*np.meshgrid(xx, xx), self.cinc(), self.pa(), axis="x")
         xxs, yys = translate_image(xxt, yyt, self.x, self.y)
         image = self.image_func(xxs, yys, pixel_size, wl)

ppdmod/components.py
@@ -242,8 +242,8 @@ class Ring(FourierComponent):
         image : astropy.units.Jy
         """
         if self.rin.unit == u.au:
-            xx = (xx.to(u.arcsec) * self.dist().to(u.pc)).value * u.au
-            yy = (yy.to(u.arcsec) * self.dist().to(u.pc)).value * u.au
+            xx = (xx * 1e-3 * self.dist()).value * u.au
+            yy = (yy * 1e-3 * self.dist()).value * u.au
 
         radius = np.hypot(xx, yy)[np.newaxis, ...]
         dx = np.max([np.diff(xx), np.diff(yy)]) * self.rin.unit
@@ -253,7 +253,7 @@ class Ring(FourierComponent):
         radial_profile = (radius >= self.rin()) & (radius <= (self.rin() + dx))
         intensity_func = kwargs.pop("intensity_func", None)
         if intensity_func is None:
-            intensity = 1 / (2 * np.pi)
+            intensity = 1 / (2 * np.pi * dx.value)
         else:
             intensity = (
                 intensity_func(radius, wl).to(u.erg / (u.cm**2 * u.rad**2 * u.s * u.Hz))

ppdmod/plot.py
@@ -114,20 +114,29 @@ def make_2D_fourier(
 
     vis, phase = np.abs(complex_vis), np.angle(complex_vis, deg=True)
     vis /= vis.max()
-    wcs = WCS(naxis=3)
-    wcs.wcs.crpix = (dim / 2, dim / 2, 1.0)
-    wcs.wcs.cdelt = (
-        np.diff(np.abs(ucoord))[0],
-        np.diff(np.abs(ucoord))[0],
-        np.diff(wls.value)[0],
-    )
-    wcs.wcs.crval = (0.0, 0.0, wls.value[0])
-    wcs.wcs.cunit = ("METER", "METER", "MICRON")
-    hdu = fits.HDUList([fits.PrimaryHDU(vis, header=wcs.to_header())])
-    hdu.writeto(savefig / "visamp.fits", overwrite=True)
 
-    hdu = fits.HDUList([fits.PrimaryHDU(phase, header=wcs.to_header())])
-    hdu.writeto(savefig / "visphi.fits", overwrite=True)
+    naxis = 2
+    crpix = (dim / 2,) * naxis
+    cdelt = (np.diff(np.abs(ucoord))[0]) * naxis
+    crval = (0.0,) * naxis
+    cunit = ("rad",) * naxis
+    if len(wls) > 1:
+        naxis += 1
+        crpix += (1.0,)
+        cdelt += (np.diff(wls)[0],)
+        crval += (wls[0],)
+        cunit += ("meter",)
+    else:
+        vis, phase = vis[0], phase[0]
+
+    # TODO: Make sure the orientation here is correct
+    wcs = WCS(naxis=naxis)
+    wcs.wcs.crpix = crpix
+    wcs.wcs.cdelt = cdelt
+    wcs.wcs.crval = crval
+    wcs.wcs.cunit = cunit
+    hdu = fits.HDUList([fits.PrimaryHDU(vis, header=wcs.to_header())])
+    hdu.writeto(savefig, overwrite=True)
 
 
 def plot_components(